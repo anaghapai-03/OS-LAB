f0cdb860650413e62508cdf2cccc4463b0d2ec3f
anaghapai-03 <anaghaa.cs24@bmsce.ac.in>
2026-03-06 12:53:08 +0530

    FCFS-Lab Program 1-06/03/2026

---
+++ FCFS/program 1.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+struct Process {
+    int id;
+    int at;
+    int bt;
+    int ct;
+    int tat;
+    int wt;
+};
+
+int main() {
+    int n, i, j;
+    float avg_wt = 0, avg_tat = 0;
+    int current_time = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    struct Process p[n], temp;
+
+    for(i = 0; i < n; i++) {
+        p[i].id = i + 1;
+        printf("Enter Arrival Time for Process %d: ", p[i].id);
+        scanf("%d", &p[i].at);
+        printf("Enter Burst Time for Process %d: ", p[i].id);
+        scanf("%d", &p[i].bt);
+    }
+
+    // Sort processes by Arrival Time
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(p[i].at > p[j].at) {
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+
+        if(current_time < p[i].at) {
+            current_time = p[i].at;
+        }
+
+        p[i].ct = current_time + p[i].bt;
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+
+        current_time = p[i].ct;
+
+        avg_wt += p[i].wt;
+        avg_tat += p[i].tat;
+    }
+
+    avg_wt /= n;
+    avg_tat /= n;
+
+    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
+               p[i].id, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", avg_wt);
+    printf("\nAverage Turnaround Time = %.2f\n", avg_tat);
+
+    return 0;
+}
